723e61dd7c15440b56c4b75804e0a617c8e41353
Tom Bell <tomb@tombell.dev>
2025-01-23 04:46:20 +0000

    Need branch = main

--- lua/tree-sitter-skbdrc/init.lua
+++ lua/tree-sitter-skbdrc/init.lua
@@ -2,6 +2,7 @@ local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.skbdrc = {
   install_info = {
     url = "https://github.com/starkwm/tree-sitter-skbdrc",
+    branch = "main",
     files = { "src/parser.c" },
   },
   filetype = "skbdrc",
