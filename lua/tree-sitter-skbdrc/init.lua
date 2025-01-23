5e92688e7ed48a50619519d7813de72b72dcf865
Tom Bell <tomb@tombell.dev>
2025-01-23 04:45:28 +0000

    Remove fields from parser setup

--- lua/tree-sitter-skbdrc/init.lua
+++ lua/tree-sitter-skbdrc/init.lua
@@ -2,10 +2,7 @@ local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.skbdrc = {
   install_info = {
     url = "https://github.com/starkwm/tree-sitter-skbdrc",
-    branch = "main",
     files = { "src/parser.c" },
-    generate_requires_npm = false,
-    requires_generate_from_grammar = false,
   },
   filetype = "skbdrc",
   used_by = { "skbdrc" },
