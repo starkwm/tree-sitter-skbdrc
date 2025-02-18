b005cff970f2b7dcb26fce9994d1a34fa6c72000
Tom Bell <tomb@tombell.dev>
2025-02-18 22:35:24 +0000

    Add tree-sitter.json

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 21
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 52
@@ -1461,7 +1461,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 146},
   [2] = {.lex_state = 4},
@@ -1945,6 +1945,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_skbdrc(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -1959,6 +1960,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_skbdrc(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
+    .name = "skbdrc",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 0,
+      .minor_version = 1,
+      .patch_version = 0,
+    },
   };
   return &language;
 }
